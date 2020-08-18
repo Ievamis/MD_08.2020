5d5360df89c227baa7dde41a65baf12a3c913e78
Ieva Misevica <ieva.misevica@gmail.com>
2020-08-18 13:34:04 +0300

    Removed FoodCalculator from Gitignore

---
+++ src/main/scala/FoodCalculator.scala
@@ -0,0 +1,12 @@
+object FoodCalculator extends App{
+  val food ="potatoes"
+  val price = 0.37
+  val quantity = 20
+  val cost = price * quantity
+
+  //print how much will it cost to buy to food
+  //It will cost xxxx to by xx kilos of xxxx
+  //I'm trying to find my FoodCalculator in GitHub.
+  println(s"It will cost $cost to by $quantity kilos of $food")
+
+}
