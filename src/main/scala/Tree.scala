d683fee79f029759a572f8dfd9d35c27d7650751
Ieva Misevica <ieva.misevica@gmail.com>
2020-08-20 11:11:23 +0300

    Homework - Tree - amended (I did it! :) )

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,9 +1,16 @@
-object Tree extends App {
- val firstRow = " "*2 + "*"
-  val secondRow = " " +"*"*3
-  val thirdRow = "*"*5
+import scala.collection.IterableOnce.iterableOnceExtensionMethods
 
-  println(firstRow)
-  println(secondRow)
-  println(thirdRow)
+object Tree extends App {
+
+ var treeHeight = 3
+
+if (!args.isEmpty) {
+ println(s"We got ${args.length} argument")
+ treeHeight = args(0).toInt
+} else
+ println("We got nothing well we will use the default value")
+
+for (i <- 1 to treeHeight*2 by 2)
+ println(" "*((treeHeight*2-i)/2) + "*"*(i+0))
+
 }
